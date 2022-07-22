f5d005c0b0f78521d6715599c1714bf9aeebe9fa
John Hashim <john.hashim@teamwave.com>
2022-07-22 17:08:43 +0530

    dom-to-image package updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import domtoimage from 'dom-to-image';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dom-to-image';
+  converToImg(){
+    domtoimage.toPng((<HTMLElement>document.getElementById('container')))
+    .then(function (data) {
+      console.log(data)
+    });
+  }
 }
